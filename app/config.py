215585c4546fd4acf19adbaa200515d9b84bc85c
Oliver Newman <oliver@whmsi.com>
2017-07-19 17:42:09 -0400

    Switch from Bottle to Flask, add some core logic

--- app/config.py
+++
@@ -1,4 +0,0 @@
-app_dir = os.environ['APP_DIR']
-images_dir = app_dir + '/images'
-bottle_port = os.environ['BOTTLE_PORT']
-redis = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
--- app/main.py
+++ app/main.py
@@ -1,16 +1,59 @@
-from bottle import Bottle, run, debug
-from redis import Redis
+from flask import Flask, send_from_directory
+from redis import StrictRedis
+from datetime import datetime, timedelta
 import os
 
-import routes
-import config
 
-app_dir = os.environ['APP_DIR']
-images_dir = app_dir + '/images'
-bottle_port = os.environ['BOTTLE_PORT']
-# redis = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
+APP_DIR = os.environ['APP_DIR']
+IMAGES_DIR = APP_DIR + '/images'
+DEBUG=True
 
-DEBUG = True
+app = Flask(__name__)
+redis = StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
 
-debug(DEBUG)
-run(host='localhost', port=config.bottle_port, reloader=DEBUG)
+
+# Helpers
+
+def is_current_image_valid():
+    current_image_timestamp = redis.get('current_image_timestamp')
+    return current_image_timestamp is not None and \
+           datetime.now() - current_image < timedelta(minutes=1)
+
+
+def is_next_image_valid():
+    # TODO
+    return True
+
+
+def random_coords():
+    lat = (random.random() - 0.5) * 160
+    lat = (random.random() - 0.5) * 360
+    return (lat, lon)
+
+
+# Routes
+
+@app.route('/')
+def index():
+    redis.incr('hits')
+    return 'RandomMap Chrome Extension' + str(redis.get('hits'))
+
+
+@app.route('/api/v1/image')
+def current_image():
+    if is_current_image_valid():
+        return send_from_directory(IMAGES_DIR, 'current_image.png')
+    elif is_next_image_valid():
+        # TODO: asynchronously copy next_image to current_image, and download a
+        # new next_image
+        return send_from_directory(IMAGES_DIR, 'next_image.png')
+    else:
+        # TODO: Gotta start somewhere
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, host='0.0.0.0')
