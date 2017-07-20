7ceba4ea7939ffe82c5d16e88ef3d78897671e45
Oliver Newman <oliver@whmsi.com>
2017-07-20 17:42:08 -0400

    Finish super-basic MVP using static files

--- app/main.py
+++ app/main.py
@@ -2,38 +2,49 @@ from flask import Flask, send_from_directory
 from redis import StrictRedis
 from mapbox import Static
 from datetime import datetime, timedelta
+import random
 import os
 
 
 DEBUG=os.environ['DEBUG']
 APP_DIR = os.environ['APP_DIR']
 IMAGES_DIR = APP_DIR + '/images'
+IMAGE_EXPIRE_TIME = {'minutes': 1}
 
 app = Flask(__name__)
 redis = StrictRedis(host=os.environ['REDIS_HOST'],
                     port=os.environ['REDIS_PORT'])
-mapbox = Static() # Must have MAPBOX_ACCESS_TOKEN set
+mapbox = Static() if 'MAPBOX_ACCESS_TOKEN' in os.environ else None
 
 
 # Helpers
-def fetch_new_image(lat, lon):
-    response = mapbox.image('mapbox.satellite', lon=lon, lat=lat, z=12)
+class RandomMapRedis(StrictRedis):
+    # TODO
+    pass
+
+
+def fetch_new_image(lat, lon, image_file):
+    response = mapbox.image('mapbox.satellite', lon=lon, lat=lat, z=12,
+                            width=1280, height=1280)
     if response.status_code != 200:
         raise
-    # TODO: 
+    with open(image_file, 'wb') as f:
+        _ = f.write(response.content)
 
 
 def is_current_image_valid():
+    return True
     return redis.exists('current_image')
 
 
 def is_next_image_valid():
+    return True
     return redis.exists('next_image')
 
 
 def random_coords():
     lat = (random.random() - 0.5) * 160
-    lat = (random.random() - 0.5) * 360
+    lon = (random.random() - 0.5) * 360
     return (lat, lon)
 
 
@@ -41,22 +52,17 @@ def random_coords():
 
 @app.route('/')
 def index():
-    redis.incr('hits')
-    return 'RandomMap Chrome Extension' + str(redis.get('hits'))
+    return 'RandomMap Chrome Extension'
 
 
 @app.route('/image')
-def current_image():
-    if is_current_image_valid():
-        # TODO: return current image
-        return send_from_directory(IMAGES_DIR, 'current_image.png')
-    elif is_next_image_valid():
-        # TODO: asynchronously copy next_image to current_image, and download a
-        # new next_image
-        return send_from_directory(IMAGES_DIR, 'next_image.png')
-    else:
-        # TODO: Gotta start somewhere
-        pass
+def image():
+    if not redis.exists('current_lat'):
+        lat, lon = random_coords()
+        redis.set('current_lat', lat, ex=60)
+        redis.set('current_lon', lon, ex=60)
+        fetch_new_image(lat, lon, IMAGES_DIR + '/current_image.png')
+    return send_from_directory(IMAGES_DIR, 'current_image.png')
 
 
 if __name__ == '__main__':
