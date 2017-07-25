bb104a89a34b59c02b0592b6ca3555a7efcfb35a
Oliver Newman <oliver@whmsi.com>
2017-07-25 08:37:36 -0400

    Don't flush Redis before starting server

--- randommap_website/main.py
+++ randommap_website/main.py
@@ -16,7 +16,6 @@ redis = StrictRedis(host=app.config['REDIS_HOST'],
                     port=app.config['REDIS_PORT'],
                     db=app.config['REDIS_DB'],
                     decode_responses=True)
-redis.flushdb() # Start with a blank slate
 mapbox = Static()
 image_context = ImageContext(app, redis, mapbox, choose_coords)
 
