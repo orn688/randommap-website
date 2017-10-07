9973a3db09644918aee6c67701bcf6fd8dd33379
Oliver Newman <oliver@whmsi.com>
2017-10-07 13:14:05 -0400

    Add access-contorl-expose-headers header

--- randommap_website/main.py
+++ randommap_website/main.py
@@ -36,12 +36,15 @@ def favicon(_):
 @app.route('/map')
 def get_map(_):
     sat_map = model.request_map()
-    randommap_headers = {
+    headers = {
         'Randommap-Latitude': sat_map.lat,
         'Randommap-Longitude': sat_map.lon,
-        'Randommap-Zoom': sat_map.zoom
+        'Randommap-Zoom': sat_map.zoom,
     }
-    return response.raw(sat_map.image, headers=randommap_headers,
+    # Allow client-side JavaScript to access custom headers
+    headers['Access-Control-Expose-Headers'] = ', '.join(h for h in headers)
+
+    return response.raw(sat_map.image, headers=headers,
                         content_type='image/png')
 
 
